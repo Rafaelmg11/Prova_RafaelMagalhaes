90211313984f16321cd11bba26e76552bbb3c697
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-08-27 20:50:45 -0300

    Ajustes
    PlaceHolders

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -47,13 +47,13 @@
     <h2>Cadastrar Usuário</h2>
     <form action="cadastro_usuario.php" method="POST" id="form_nome" >
         <label for="nome">Nome: </label>
-        <input type="text" id="nome" name="nome" pattern="[A-Za-zÀ-ÿ0-9 ]+" title="Não é permitido usar símbolos."  required>
+        <input type="text" id="nome" name="nome" pattern="[A-Za-zÀ-ÿ0-9 ]+" title="Não é permitido usar símbolos."  placeholder="Digite o nome" required>
 
         <label for="email">E-mail: </label>
-        <input type="email" id="email" name="email" required>
+        <input type="email" id="email" name="email" required placeholder="Digite o E-mail">
 
         <label for="senha">Senha: </label>
-        <input type="password" id="senha" name="senha" required>
+        <input type="password" id="senha" name="senha" required placeholder="Digite a senha">
 
         <label for="id_perfil">Perfil: </label>
         <select name="id_perfil" id="id_perfil">
